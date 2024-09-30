8900a2605547eb0a4cc591c598121aad20156b52
Oniely <jabagatniel09@gmail.com>
2024-09-30 23:26:14 +0800

    stock inventory

---
+++ resources/js/Components/dashboard/StockSlot.tsx
@@ -0,0 +1,91 @@
+import { useState } from 'react';
+import PrimaryButton from '../PrimaryButton';
+import SecondaryButton from '../SecondaryButton';
+import Modal from '../Modal';
+
+interface Props {
+  name: string;
+  isAvailable?: boolean;
+  variety: string;
+  stock: number;
+  onUpdate?: () => void;
+  onRelease?: () => void;
+}
+
+export default function StockSlot({
+  name,
+  isAvailable = false,
+  variety,
+  stock,
+  onUpdate,
+  onRelease,
+}: Props) {
+  const [modalOpen, setModalOpen] = useState(false);
+  const [modalType, setModalType] = useState<'update' | 'release' | null>(null);
+
+  const handleOpenModal = (type: 'update' | 'release') => {
+    setModalType(type);
+    setModalOpen(true);
+  };
+
+  const handleCloseModal = () => {
+    setModalOpen(false);
+    setModalType(null);
+  };
+
+  return (
+    <>
+      <div className="flex flex-col items-center w-60 gap-6 py-4 px-5 border-2 rounded-xl border-primary">
+        <div className="text-center">
+          <h3 className="text-xl font-semibold leading-4 uppercase">{name}</h3>
+          <span
+            className={`text-sm font-medium ${
+              isAvailable ? 'text-primary' : 'text-[#ff2b2b]'
+            }`}
+          >
+            {isAvailable ? 'Available' : 'Not available'}
+          </span>
+        </div>
+        <div className="space-y-3">
+          <div className="text-center">
+            <h4 className="text-lg font-semibold leading-4 text-primary">
+              {variety}
+            </h4>
+            <span className="text-sm text-t-light">Variety</span>
+          </div>
+          <div className="text-center">
+            <h4 className="text-lg font-semibold leading-4 text-primary">
+              {stock} kg
+            </h4>
+            <span className="text-sm text-t-light">Current Stocks</span>
+          </div>
+        </div>
+        <div className="flex flex-col items-center gap-2 w-full">
+          <PrimaryButton className="w-full py-3" onClick={() => handleOpenModal('update')}>
+            Update
+          </PrimaryButton>
+          <SecondaryButton
+            className="w-full bg-t-light text-[#ff2b2b] py-3"
+            onClick={() => handleOpenModal('release')}
+          >
+            Release
+          </SecondaryButton>
+        </div>
+      </div>
+
+      <Modal show={modalOpen} onClose={handleCloseModal} maxWidth='lg'>
+        {modalType === 'update' && (
+          <div>
+            <h2>Update Stock</h2>
+          </div>
+        )}
+
+        {modalType === 'release' && (
+          <div>
+            <h2>Release Stock</h2>
+          </div>
+        )}
+      </Modal>
+    </>
+  );
+}
--- resources/js/Pages/Stock/Index.tsx
+++ resources/js/Pages/Stock/Index.tsx
@@ -1,3 +1,4 @@
+import StockSlot from '@/Components/dashboard/StockSlot';
 import { Tab, Tabs } from '@/Components/Tabs';
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
 import { Head } from '@inertiajs/react';
@@ -7,10 +8,51 @@ export default function Index() {
     <AuthenticatedLayout header="Stock Inventory">
       <Head title="Stock Inventory" />
 
-      <div className="">
+      <div className='flex items-center justify-around flex-wrap max-md:flex-col gap-6'>
+        <StockSlot name='Slot 1' variety='RC1' stock={20} isAvailable />
+        <StockSlot name='Slot 2' variety='RC1' stock={20} isAvailable />
+        <StockSlot name='Slot 3' variety='RC1' stock={20} isAvailable />
+      </div>
+
+      <div className="mt-14">
         <Tabs>
           <Tab label="ACTIVITY">
-            <p>This is the content of Activity.</p>
+            <div className="relative overflow-x-auto">
+              <table className="w-full text-sm text-left">
+                <thead className="text-xs text-gray-900 uppercase">
+                  <tr className="border-b">
+                    <th scope="col" className="px-6 py-3">
+                      Date
+                    </th>
+                    <th scope="col" className="px-6 py-3">
+                      Time
+                    </th>
+                    <th scope="col" className="px-6 py-3">
+                      Activity
+                    </th>
+                    <th scope="col" className="px-6 py-3">
+                      Slot
+                    </th>
+                    <th scope="col" className="px-6 py-3" />
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr
+                    className="border-b hover:bg-green-100 text-t-light font-medium"
+                  >
+                    <td className="px-6 py-3">
+                      09-03-24
+                    </td>
+                    <td className="px-6 py-3">
+                      12:00PM
+                    </td>
+                    <td className="px-6 py-3">RESTOCK</td>
+                    <td className="px-6 py-3">SLOT 1</td>
+                    <td className="px-6 py-3">...</td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
           </Tab>
           <Tab label="Tab 2">
             <p>This is the content of Tab 2.</p>
