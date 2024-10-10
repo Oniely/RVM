d28f333ad137adca3269f8be9d98e813eb971683
Oniely <jabagatniel09@gmail.com>
2024-10-10 23:19:36 +0800

    config app name, auth layout, env

--- resources/js/Layouts/AuthenticatedLayout.tsx
+++ resources/js/Layouts/AuthenticatedLayout.tsx
@@ -3,12 +3,14 @@ import Sidebar from '@/Components/shared/Sidebar';
 import SettingDropdown from '@/Components/shared/SettingDropdown';
 import NotificationDropdown from '@/Components/shared/NotificationDropdown';
 import MobileNavbar from '@/Components/shared/MobileNavbar';
-import { Link } from '@inertiajs/react';
+import { Link, usePage } from '@inertiajs/react';
 
 export default function AuthenticatedLayout({
   header,
   children,
 }: PropsWithChildren<{ header?: string | ReactNode }>) {
+  const { appName } = usePage().props;
+
   return (
     <div className="flex flex-row min-h-screen max-md:flex-col max-container">
       <Sidebar />
@@ -17,7 +19,7 @@ export default function AuthenticatedLayout({
       <main className="flex flex-col flex-1 min-h-screen px-4 pt-5 pb-10 sm:px-6 md:pt-12 md:px-14">
         <header className="pt-6 flexBetween md:border-t md:border-gray-200">
           <h2 className="text-4xl font-semibold leading-tight text-gray-800">
-            {header}
+            {header || `${appName}`}
           </h2>
           <div className="gap-4 flexCenter">
             <Link
